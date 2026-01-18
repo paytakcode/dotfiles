1ed7d556de2091cc4e16149e9bdfdffce55b2faa
paytakcode <paytakcode@gmail.com>
2026-01-18 16:12:55 +0900

    add fish config for mise(rails)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -34,3 +34,6 @@ alias lg='lazygit'
 alias pacman='pacman --noconfirm'
 alias yay='yay --noconfirm --sudoloop'
 alias !! execute_last_command
+fish_add_path $HOME/.local/bin
+mise activate fish | source
+direnv hook fish | source
